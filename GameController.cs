cd38102987be038d18d860ec213575ba0016f1b6
Sedat Kapanoglu <ssg@sourtimes.org>
2018-08-12 23:18:50 -0700

    add APL headers

--- GameController.cs
+++ GameController.cs
@@ -1,6 +1,19 @@
-﻿using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
+﻿/*
+Copyright 2018 Sedat Kapanoglu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System.ComponentModel;
 using System.Threading;
 
 namespace NoJoy
@@ -13,7 +26,7 @@ namespace NoJoy
         Enabling,
     }
 
-    class GameController: INotifyPropertyChanged
+    class GameController : INotifyPropertyChanged
     {
         private const string standardArguments = "-InformationAction SilentlyContinue -PassThru -Confirm:$false";
         public string Name { get; set; }
@@ -21,6 +34,7 @@ namespace NoJoy
         public string DeviceId { get; set; }
 
         private GameControllerState state;
+
         public GameControllerState State
         {
             get { return state; }
--- WmiControllerEnumerator.cs
+++ WmiControllerEnumerator.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+Copyright 2018 Sedat Kapanoglu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
@@ -8,8 +23,7 @@ namespace NoJoy
     internal class WmiControllerEnumerator
     {
         private const string gameControllerId = "HID_DEVICE_SYSTEM_GAME";
-
-        private const string hidQuery =
+        private const string hidQuery = 
             @"SELECT Manufacturer,Name,HardwareID,DeviceID,Status FROM Win32_PnPEntity
               WHERE PNPClass='HIDClass'";
         private const string saitekHidSuffix = " (HID)";
