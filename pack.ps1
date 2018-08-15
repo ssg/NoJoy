67ce795acd1420ce9fb21b4c3fa98acd5b976bcb
Sedat Kapanoglu <ssg@sourtimes.org>
2018-08-14 18:53:11 -0700

    add helper tool for packing releases

---
+++ pack.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+Pack the build output into a ZIP file for release upload
+
+.PARAMETER Version
+Pack and name the ZIP file according to the version
+
+.PARAMETER BuildOutputDirectory
+Where the build artifacts are located.
+#>
+
+param (
+   [Parameter(Mandatory=$true)][string]$Version,
+   [string]$BuildOutputDirectory = "bin\Release"
+)
+
+$files = "NoJoy.exe","NoJoy.exe.config","LICENSE" 
+$filePaths = ($files | foreach { Join-Path $BuildOutputDirectory $_ })
+$zipPath= (Join-Path $BuildOutputDirectory "NoJoy-$Version.zip")
+
+Compress-Archive -Force -Compression Optimal -Path $filePaths -DestinationPath $zipPath
