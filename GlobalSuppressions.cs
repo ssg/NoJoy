9be8c192239e053deec658f76893edce0d49fe76
Sedat Kapanoglu <ssg@sourtimes.org>
2018-08-14 19:30:43 -0700

    add code cleanup related files for vs

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0049:Simplify Names", Justification = "<Pending>", Scope = "member", Target = "~M:NoJoy.ErrorVisibilityConverter.Convert(System.Object,System.Type,System.Object,System.Globalization.CultureInfo)~System.Object")]
+
